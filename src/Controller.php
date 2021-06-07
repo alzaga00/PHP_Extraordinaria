1846112e3b00a4403d73888cc93b61aa44e47386
alzaga00 <60656352+alzaga00@users.noreply.github.com>
2021-06-07 12:49:55 +0200

    Add files via upload

---
+++ src/Controller.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Client;
+
+
+class ClientController extends Controller
+{
+
+    public function index()
+    {
+        return Client::all();
+    }
+
+    public function show(Client $client)
+    {
+        return $client;
+    }
+
+    public function store(Request $request)
+    {
+        $client = Client::create($request->all());
+
+        return response()->json($client, 201);
+    }
+
+    public function update(Request $request, Client $client)
+    {
+        $client->update($request->all());
+
+        return response()->json($client, 200);
+    }
+
+    public function delete(Client $client)
+    {
+        $client->delete();
+
+        return response()->json(null, 204);
+    }
+}
